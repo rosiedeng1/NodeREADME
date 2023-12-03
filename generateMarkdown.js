dedb13641a3a475fd3490b5e3adc8af18bddd0e3
rosiedeng1 <rd25500@gmail.com>
2023-12-03 15:02:40 -0500

    added license in generateMarkdown js

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,22 +1,61 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  
+  // Check if a license is specified
+  if (license !== 'None') {
+    // If a license is specified, return a string with the license badge URL
+    return `![GitHub licens badge](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  }
+  // If no license is specified, return an empty string
+  return '';
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+   // Check if a license is specified
+   if (license !== 'None') {
+    // If a license is specified, return a string with the license badge URL
+    return 'https://choosealicense.com/](https://choosealicense.com/)';
+  }
+  // If no license is specified, return an empty string
+  return '';
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'None') {
+    return 'Here is the ${license} section';
+  }
+  return '';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description
+
+  ## Table of Contents (Optional)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+  ## Installation
+  ## Usage
+  ## Credits
+  ## License
+  ## Badges
+  ## How to Contribute
+  ## Tests
+
 `;
 }
+
+renderLicenseBadge(license)
+renderLicenseLink(license)
+renderLicenseSection(license)
+generateMarkdown(data)
 
 module.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -1,41 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-
-const generateHTML = ({ title, description, installation, usage, contributing, test, license, username, email }) =>
-  `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
-  <header class="p-5 mb-4 header bg-light">
-    <div class="container">
-      <h1 class="display-4">${title}</h1>
-      <h2> Example heading <span class="badge bg-secondary">Description </span><h2> 
-      <p class="lead"> ${description}.</p>
-      <h3> Table of Contents <h3> 
-      <h4> Installation <h4> 
-      <p>${installation}.</p>
-      <h5> Usage <h5> 
-      <p>${usage}.</p>
-      <h7> Contributing <h7> 
-      <p>${contributing}.</p>
-      <h8> Tests <h8> 
-      <p>${test}.</p>
-      <h6> License <h6> 
-      <p>${license}.</p>
-      <h9> Questions <h9> 
-      <p>${username}.</p>
-      <p>${email}.</p>
-
-    </div>
-  </header>
-</body>
-</html>`;
+const generateMarkdown = require('generateMarkdown')
 
 // TODO: Create an array of questions for user input
 const questions = [{
@@ -46,7 +12,7 @@ const questions = [{
   {
     type: 'input',
     name: 'description',
-    message: 'Please write a short description of the what, why and how for your project',
+    message: 'What is the purpose of your project? (description)',
   },
   {
     type: 'input',
@@ -56,22 +22,22 @@ const questions = [{
   {
     type: 'input',
     name: 'usage',
-    message: 'Please provide instructions and examples on how to use your application',
+    message: 'How do you use your application?',
   },
   {
     type: 'input',
     name: 'contributing',
-    message: 'Please include guidelines on how others can contribute to your project',
+    message: 'How can others contribute to your project?',
   },
   {
     type: 'input',
     name: 'test',
-    message: 'Please provide instructions on how to run your tests for your application ',
+    message: 'How do you run your tests for your application?',
   },
   {
     type: 'list',
     name: 'license',
-    message: 'Please choose a license for your application ',
+    message: 'What is the license for your application?',
   },
   {
     type: 'input',
